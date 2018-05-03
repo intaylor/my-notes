da465a998614c4010aed92916905e3cab9d14368
untaylor <w_zhenhua@yahoo.com>
2018-05-03 16:33:08 -0700

    First commit

---
+++ src/main/java/com/mynotes/Note.java
@@ -0,0 +1,37 @@
+package com.mynotes;
+
+public class Note {
+	public int id;
+	public String title;
+	public String content;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Note [title=" + title + ", content=" + content + "]";
+	}
+
+}
